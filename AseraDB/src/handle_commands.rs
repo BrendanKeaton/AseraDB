78f0e27732232330d41359b2a36fb3b5d2c011f4
Brendan Keaton <brendanrkeaton@gmail.com>
2026-01-31 18:53:25 -0500

    feat: add scalable loop with match instead of if/else, add basic error handling in select

--- AseraDB/src/handle_commands.rs
+++ AseraDB/src/handle_commands.rs
@@ -2,13 +2,34 @@ use crate::enums::TokenType;
 use crate::utils::classify_token;
 use crate::{enums::Command, structs::QueryObject};
 
-pub fn handleSelect(tokens: &Vec<&str>, query: &mut QueryObject) {
+pub fn handle_select(tokens: &Vec<&str>, query: &mut QueryObject) {
     query.command = Some(Command::SELECT);
     query.index += 1;
-
     while let TokenType::VALUE(val) = classify_token(tokens[query.index]) {
-        // val contains the ValueTypes enum value here
         query.fields.push(val);
         query.index += 1;
+
+        if query.index == query.length {
+            println!(
+                "Malformed Request. Please Complete Command. Commands cannot end with SELECT query."
+            );
+            return;
+        }
+    }
+}
+
+pub fn handle_create(tokens: &Vec<&str>, query: &mut QueryObject) {
+    query.command = Some(Command::CREATE);
+    query.index += 1;
+    while let TokenType::VALUE(val) = classify_token(tokens[query.index]) {
+        query.fields.push(val);
+        query.index += 1;
+
+        if query.index == query.length {
+            println!(
+                "Malformed Request. Please Complete Command. Commands cannot end with SELECT query."
+            );
+            return;
+        }
     }
 }
--- AseraDB/src/utils.rs
+++ AseraDB/src/utils.rs
@@ -3,26 +3,28 @@ use crate::{
     structs::QueryObject,
 };
 
-use crate::handle_commands::handleSelect;
+use crate::handle_commands::handle_select;
 
 pub fn handle_sql_inputs(input: &str) -> bool {
     let tokens: Vec<&str> = input.split_whitespace().collect();
 
     let mut query: QueryObject = QueryObject::default();
 
-    query.length = tokens.len() - 1;
+    query.length = tokens.len();
     query.index = 0;
 
-    while query.index <= query.length {
+    while query.index < query.length {
         let curr_token: TokenType = classify_token(tokens[query.index]);
-        if let TokenType::CMD(cmd) = curr_token {
-            if cmd == Command::SELECT {
-                handleSelect(&tokens, &mut query);
+        match curr_token {
+            TokenType::CMD(command) => {
+                match_command(&command, &tokens, &mut query);
             }
+            TokenType::OP(operand) => todo!(),
+            TokenType::FILTER(filter) => todo!(),
+            TokenType::VALUE(value_types) => todo!(),
+            TokenType::SYNTAX(syntax) => todo!(),
         }
-        println!("{}", query);
-        println!("{:?}", tokens[query.index]);
-        query.index += 1;
+        println!("{}", query)
     }
 
     return true;
@@ -36,4 +38,14 @@ pub fn classify_token(token: &str) -> TokenType {
         .or_else(|| Syntax::from_str(token).map(TokenType::SYNTAX))
         .or_else(|| ValueTypes::from_str(token).map(TokenType::VALUE))
         .unwrap_or_else(|| return TokenType::CMD(Command::EXIT))
+}
+
+fn match_command(command: &Command, tokens: &Vec<&str>, query: &mut QueryObject) {
+    match command {
+        Command::SELECT => handle_select(tokens, query),
+        Command::INSERT => todo!(),
+        Command::DELETE => todo!(),
+        Command::CREATE => todo!(),
+        Command::EXIT => todo!(),
+    }
 }
